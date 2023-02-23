060d5aeebb8775a316974db6806d05cc672ab295
idilipsarker <dilipsarker719@gmail.com>
2023-02-23 12:59:40 +0600

    some route api added

--- index.js
+++ index.js
@@ -4,17 +4,35 @@ const cors = require("cors");
 const port = process.env.port || 5000;
 
 //middleware
-
 app.use(cors());
+
+//import news data
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.get("/", (req, res) => {
   res.send("new api running");
 });
 
-const categories = require("./data/categories.json");
-
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+  console.log(selectedNews);
 });
 
 app.listen(port, () => {
